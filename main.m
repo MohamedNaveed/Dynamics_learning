fb8fa45ac6e18d08dd615e1b18d005240c0566c4
MohamedNaveed <mohdnaveed96@gmail.com>
2023-08-24 11:50:43 -0500

    phase portrait added

--- main.m
+++ main.m
@@ -23,16 +23,48 @@ end
 
 x_max = max(x,[],2); % max value for normalization.
 
+%% find the error between the first cycle and the future cycles.
+
+T = 12; %number of time steps in a period. 1/model.dt for small angles. 12 for 90 deg x(0).
+x_cycle = x(:,1:T);
+error_true_evol = zeros(size(x));
+
+for iT = 1:floor(t_steps/T)
+    
+    error_true_evol(:,(iT-1)*T+1:iT*T) = x(:,(iT-1)*T+1:iT*T) - x_cycle;
+        
+end
+error_true_evol(:,iT*T + 1:end) =  x(:,iT*T + 1:end) - x_cycle(:,1:mod(t_steps+1,T));
+
+error_true_evol = error_true_evol./x_max;
+
+%% plot the data.
+
+figure(10);
+subplot(2,1,1);
+hold on;
+plot(0:model.dt:t_span, error_true_evol(1,:),'b','LineWidth',2); 
+ylabel('error - theta');
+title('Error between first cycle and true data');
+
+subplot(2,1,2);
+hold on;
+plot(0:model.dt:t_span, error_true_evol(2,:),'b','LineWidth',2); 
+ylabel('error - theta dot');
+xlabel('time');
 
 %% plot the simulated data.
 
+t_span_plot = 20;
+t_step_plot = t_span_plot/model.dt + 1; 
+t_idxs = (0:t_step_plot-1)*model.dt;
 fig = figure(1);
 subplot(2,1,1);
-plot(0:model.dt:t_span, x(1,:),'LineWidth',2); 
+plot(t_idxs, x(1,1:t_step_plot),'LineWidth',2); 
 ylabel('theta');
 title('Response of the system');
 subplot(2,1,2);
-plot(0:model.dt:t_span, x(2,:),'LineWidth',2); 
+plot(t_idxs, x(2,1:t_step_plot),'LineWidth',2); 
 ylabel('theta dot');
 xlabel('time');
 
@@ -41,6 +73,14 @@ screenposition = get(fig,'Position');
 set(fig,...
     'PaperPosition',[0 0 screenposition(3:4)],...
         'PaperSize',[screenposition(3:4)]);
+
+fig = figure(7);
+plot(x(1,1:t_step_plot),x(2,1:t_step_plot),'LineWidth',0.5);
+ylabel('theta dot');
+xlabel('theta');
+title('Phase portrait');
+
+%% error between every cycle of the data.
 
 %% exact DMD
 
@@ -103,7 +143,7 @@ xlabel('time');
 
 %% Hankel/Window DMD
 
-window = 20; % window / time delayed samples considered for Hankel DMD.
+window = 10; % window / time delayed samples considered for Hankel DMD.
 n_samples = 81; % training samples columns of X
 
 X = zeros(model.nx*window,n_samples);
@@ -197,7 +237,7 @@ y = ylim; % current y-axis limits
 x_idx = (window)*model.dt;
 plot([x_idx  x_idx],[y(1) y(2)],'k','LineWidth',2, 'DisplayName', 'Predictions start');
 ylabel('error - theta');
-title('Error between wDMD predictions and true data');
+title('Error between wDMD and truth');
 legend();
 
 subplot(2,1,2);
@@ -235,23 +275,31 @@ xlabel('time');
 %saveas(fig,'wDMD_w_20_x0_90.pdf')
 
 %% Comparison with Autoregressive model.
-%{ 
+ 
  
 
 %% Autoregressive AR model. 
 
-X = zeros(model.nx*window,n_samples);
-
-% creating data matrix
-for w = 1:window
-    
-    X(model.nx*(w-1) +1:model.nx*w,1:n_samples) = x(:,window - (w - 1) : ...
-                                           window - (w - 1) + n_samples -1);
-end
+%X is same as wDMD
 
 Xprime_arma = x(:,window + 1 : window + n_samples );
+
+if thresh == 1
+    A_arma = Xprime_arma*pinv(X); %exact reconstruction of A (includes all the modes)
+else
+    % finding the reduced number of modes to meet the threshold (thresh) value.
+    for S_i = 1:length(diag_S)
+   
+        sum_S_i = sum(diag_S(1:S_i)); % partial sum of all the singular values. 
     
-A_arma = Xprime_arma*pinv(X);
+        if sum_S_i/sum_S >= thresh
+            r = S_i;
+            break;
+        end      
+    end
+    
+    A_arma = Xprime_arma*V(:,1:r)*inv(S(1:r,1:r))*U(:,1:r)'; % A calculated using reduced modes.
+end
 
 %% prediction using A
 
@@ -282,13 +330,13 @@ figure(6);
 
 subplot(2,1,1);
 hold on;
-plot(0:model.dt:t_span, error(1,:)./x_max(1),'b','LineWidth',2); 
+plot(0:model.dt:t_span, error(1,:),'b','LineWidth',2); 
 ylabel('error - theta');
-title('Error between AR predictions and true data');
+title('Error between AR and true data');
 
 subplot(2,1,2);
 hold on;
-plot(0:model.dt:t_span, error(2,:)./x_max(2),'b','LineWidth',2); 
+plot(0:model.dt:t_span, error(2,:),'b','LineWidth',2); 
 ylabel('error - theta dot');
 xlabel('time');
 
@@ -303,11 +351,11 @@ figure(7);
 subplot(2,1,1);
 title('Error between ARMA and wDMD');
 hold on;
-plot(0:model.dt:t_span, error(1,:)./x_max(1),'b','LineWidth',2); 
+plot(0:model.dt:t_span, error(1,:),'b','LineWidth',2); 
 ylabel('error - theta');
 subplot(2,1,2);
 hold on;
-plot(0:model.dt:t_span, error(2,:)./x_max(2),'b','LineWidth',2); 
+plot(0:model.dt:t_span, error(2,:),'b','LineWidth',2); 
 ylabel('error - theta dot');
 xlabel('time');
 %}
